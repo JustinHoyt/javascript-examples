d1eb24cfcac36c49b701afaf82d5a4cab47ff4ae
Justin Hoyt <hoyjusti@amazon.com>
2020-11-06 13:01:11 -0800

    Add enum example

---
+++ src/enums/directions.js
@@ -0,0 +1,16 @@
+/**
+ * @enum {{
+ *   NORTH: string,
+ *   SOUTH: string,
+ *   EAST: string,
+ *   WEST: string,
+ * }}
+ */
+const directions = Object.freeze({
+  NORTH: 'north',
+  SOUTH: 'south',
+  EAST: 'east',
+  WEST: 'west',
+});
+
+export default directions;
