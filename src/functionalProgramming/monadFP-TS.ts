0f12be1972d1064feb6f09effafab8b6d810d9d9
Justin Hoyt <hoyjusti@amazon.com>
2020-11-14 18:26:11 -0800

    Add flow examples to monadFP-TS

--- src/functionalProgramming/monadFP-TS.ts
+++ src/functionalProgramming/monadFP-TS.ts
@@ -1,37 +1,75 @@
 import * as O from 'fp-ts/lib/Option';
-/* eslint-disable no-multiple-empty-lines */
-/// <reference path="../../types/node.d.ts"/>
-import * as R from 'ramda';
 
-import { chain, map, option } from 'fp-ts';
-import { flow, pipe } from 'fp-ts/lib/function';
+import { constant, flow } from 'fp-ts/lib/function';
 
-const log = (prefix: string) => (x) => console.log(prefix, x);
+import { Option } from 'fp-ts/lib/Option';
 
-const obj = {
-  addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: 'WC2N' }],
+const optionalProp = <P extends keyof T, T>(prop: P) => (obj: T): O.Option<T[P]> => O.of(obj[prop]);
+
+interface MyMap {
+  addresses: [
+    {
+      street: { name: string, number: number },
+      postcode: string,
+    }
+  ]
 }
 
-const getAddress = flow(
+const log = (prefix: string) => (x: unknown) => console.log(prefix, x);
+
+const obj: MyMap = {
+  addresses: [
+    {
+      street: { name: 'Mulburry', number: 8402 },
+      postcode: 'WC2N'
+    }
+  ],
+}
+
+const getAddressMapping: (obj: MyMap) => Option<Option<Option<Option<void>>>> =
+flow(
   O.fromNullable,
-  O.map((object) => object['addresses']),
+  O.map(optionalProp('addresses')),
+  O.map(O.map(optionalProp(0))),
+  O.map(O.map(O.map(optionalProp('street')))),
+  O.map(O.map(O.map(O.map(log('mapping: '))))),
 );
+// that's a lot of mapping over Options!
 
-const getAddressResult = getAddress({
-  addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: 'WC2N' }],
-});
+const addressMapping = getAddressMapping(obj);
 
-O.fold(() => console.log('failed'), log('succeeded: '))(getAddressResult);
 
-const street = pipe(O.of({
-  addresses: [{ street: { name: 'Mulburry', number: 8402 }, postcode: 'WC2N' }],
-}),
+
+
+
+// chain is a map and join combined, keeping monads of the same type from nesting.
+const getAddressChaining: (obj: MyMap) => Option<unknown> =
+flow(
+  O.fromNullable,
+  O.chain(optionalProp('addresses')),
+  O.chain(optionalProp(0)),
+  O.chain(optionalProp('street')),
+  O.fold(() => undefined, (street) => log('chain: ')(street))
+);
+getAddressChaining(obj);
+// The typing is limited when understanding how to operate over the values in
+// the monads as they move through the flow operator because they can lose
+// context of previous computed values. For sitations like that use the bind
+// syntax, similar in concept to the Do syntax in Haskell
+
+
+
+
+
+// do/bind syntax is more flexible than chain because we can bind variables to
+// the context of the `do` as it goes through the actions.
+const getAddressBinding: (obj: MyMap) => Option<string> =
+flow(O.fromNullable,
   O.bindTo('object'),
   O.bind('addresses', ({object}) => O.of(object.addresses)),
   O.bind('firstAdress', ({addresses}) => O.of(addresses[0])),
   O.bind('street', ({firstAdress}) => O.of(firstAdress.street)),
-  O.fold(() => undefined, ({street}) => street)
+  O.fold(() => undefined, ({street}) => log('do: ')(street))
 );
 
-console.log(getAddressResult);
-console.log(street);
+getAddressBinding(obj)
